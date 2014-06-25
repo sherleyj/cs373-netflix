eccf3d90d76f02157ba578f815166e36e3cad6ef
Sherley Jones <sherleyjones@utexas.edu>
2014-06-25 12:06:59 -0500

    fixes #3 mostly, still need to add more and modified all functions

--- Netflix.py
+++ Netflix.py
@@ -5,38 +5,101 @@
     -must predict that movie's rating for each customer.
 """
 import json, sys
+from functools import reduce
+from math      import sqrt
+from sys       import version
+from time      import clock
+
+def sqre_diff (x, y) :
+    return (x - y) ** 2
 
 
-"""
+def netflix_read_json (key, file_path) :
+    """
     read in one line.
-"""
-def collatz_read () :
-    jsonFile = open("/u/sj8346/cs373/netflix-tests/mwham-probeRatings.json", 'r')
+    """
+    jsonFile = open(file_path, 'r')
     customersDict = json.loads(jsonFile.read())
-    customerRating = (customersDict["1024"])
+    customerRating = (customersDict[key])
     jsonFile.close()
     
     return customerRating
 
-"""
-    compute root mean Root Mean Square Deviation
-"""
-def rmsr (a, p) :
-    return 1
+def netflix_read (r) :
+    """
+    r is a reader, read a line containing 
+    on int and return it. If movieID, 
+    removes the ':' at the end.
+    """
+    s = r.readline()
+    if s == "" :
+        return []
+    return s
+    #return s.strip(':')
 
-"""
-    compute prediction for what a cutomer will
-    rate a given movie
-"""
+def netflix_is_movie (s) :
+    s = s[::-1]
+    return s.startswith(':')
+
+
+def netflix_write (w, v) :
+    """
+    write to json file
+    
+    with open(file_path, 'w') as outfile:
+        json.dump(data, outfile)
+    outfile.close()
+    """
+    w.write(str(v) + "\n") 
+
+def rmse_zip_reduce (a, p) :
+    """
+    compute root mean square deviation
+    O(1) in space
+    O(n) in time
+    """
+    assert(hasattr(a, "__len__"))
+    assert(hasattr(p, "__len__"))
+    assert(hasattr(a, "__iter__"))
+    assert(hasattr(p, "__iter__"))
+    assert(len(a) == len(p))
+    s = len(a)
+    v = sum(map(sqre_diff, a, p))
+    return sqrt(v / s)
+
+
 def netflix_eval (customer, movie) :
-    return 1
+    """
+    compute prediction for what a customer will
+    rate a given movie
+    """        
+    customer_avg  = netflix_read_json(customer, 
+    "/u/mukund/netflix-tests/bryan-customer_cache.json")
+    movie_avg = netflix_read_json(movie, 
+    "/u/mukund/netflix-tests/rbrooks-movie_average_rating.json")    
+        
 
-"""
-    r is reader
-    w is writer
-"""
+    p_movie_rating = int(customer_avg + movie_avg)/2
+    
+    return p_movie_rating
+
+
 def netflix_solve (r, w) :
-    return 1
+    """
+    r is input file with movie ID, followed by customers
+    that rated that movie.
+    """
+    movie = "1"
+    customer = "1" 
+    rating  = "1"
+    while True : 
+        a = netflix_read(r)
+        if netflix_is_movie(a) :
+            netflix_write(w, a)
+            movie = a.strip(':')
+        else :
+            customer = a
+            rating = netflix_eval (customer, movie)
+            netflix_write (w, rating)
 
 
-print( collatz_read())
--- TestNetflix.py
+++ TestNetflix.py
@@ -3,3 +3,72 @@
 # not sure if we can use io to test with json files
 from io       import StringIO
 from unittest import main, TestCase
+from Netflix  import netflix_read_json, netflix_write, netflix_eval, netflix_is_movie, netflix_solve
+
+class TestNetflix (TestCase) :
+
+    # ----
+    # read
+    # ----
+
+    def test_read_json (self) :
+        key = "291"
+        file_path = "/u/mukund/netflix-tests/bryan-customer_cache.json"
+        j = netflix_read_json(key, file_path)
+        self.assertEqual(j, 3.48)
+    
+    def test_read_json_1 (self) :
+        key = "3926"
+        file_path = "/u/mukund/netflix-tests/eros-movie_cache.json"
+        j = netflix_read_json(key, file_path)
+        self.assertEqual(j,["2.5", "0.69"])
+
+    # -----
+    # write
+    # -----
+   
+    def test_write (self) :
+        w = StringIO()
+        data = "4590:"
+        netflix_write(w, data)
+        self.assertEqual(w.getvalue(), "4590:\n")
+
+    def test_write_1 (self) :
+        w = StringIO()
+        data = "3290"
+        netflix_write(w, data)
+        self.assertEqual(w.getvalue(), "3290\n")
+
+    # ----
+    # eval
+    # ----
+    
+    def test_eval (self) :
+        customer = "6"
+        movie = "3926"
+        j = netflix_eval(customer, movie)
+        #self.assertEqual(j, 3.42)
+
+
+    def test_is_movie (self) :
+        s = "4:"
+        j = netflix_is_movie(s)
+        self.assertEqual(j, True)
+
+    def test_is_movie_1 (self) :
+        s = "24"
+        j = netflix_is_movie(s)
+        self.assertEqual(j, False)
+
+    # -----
+    # solve
+    # -----
+
+    def test_solve (self) :
+        r = StringIO("2043:\n1417435\n2312054\n462685\n10851:\n1417435\n2312054\n462685")
+        w = StringIO()
+        netflix_solve(r, w)
+        self.assertEqual(w.getvalue(), "2043:\n3.4\n4.1\n1.9\n10851:\n4.3\n1.4\n2.8")
+
+main()
+
